3b587351c8e5ef6ccaf146007f20e40b37dfa336
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-01-30 14:38:45 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import "./styles.css";
+import Axios from "axios";
+import { v4 as uuidv4 } from "uuid";
+import Recipe from "./components/Recipe";
+import Alert from "./components/Alert";
+
+export default function App() {
+  const [query, setQuery] = useState("");
+  const [recipes, setRecipes] = useState([]);
+  const [alert, setAlert] = useState("");
+  const APP_ID = "5f64e117";
+  const APP_KEY = "51ec8574fe75b470e5e0ee66c6c845f6";
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  const getData = async () => {
+    if (query !== "") {
+      const result = await Axios.get(url);
+      if (!result.data.more) {
+        return setAlert("No food with such name");
+      }
+      console.log(result);
+      setRecipes(result.data.hits);
+      setQuery("");
+      setAlert("");
+    } else {
+      setAlert("Please fill the form");
+    }
+  };
+
+  const onChange = (e) => setQuery(e.target.value);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    getData();
+  };
+
+  return (
+    <div className="App">
+      <h1 className="head">Feed~Tasteful </h1>
+      <form onSubmit={onSubmit} className="search-form">
+        {alert !== "" && <Alert alert={alert} />}
+        <input
+          type="text"
+          name="query"
+          onChange={onChange}
+          value={query}
+          autoComplete="off"
+          placeholder="Search Food"
+        />
+        <input type="submit" value="Search" />
+      </form>
+      <div className="recipes">
+        {recipes !== [] &&
+          recipes.map((recipe) => <Recipe key={uuidv4()} recipe={recipe} />)}
+      </div>
+    </div>
+  );
+}
